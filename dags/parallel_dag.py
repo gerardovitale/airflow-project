0f9d5f77d1680b076a243ec17d8ce802cc8aa0d8
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2023-02-10 18:34:24 +0100

    add echo command to parallel_dag.py

--- dags/parallel_dag.py
+++ dags/parallel_dag.py
@@ -11,27 +11,27 @@ with DAG(
 ) as dag:
     extract_a = BashOperator(
         task_id='extract_a',
-        bash_command='sleep 1'
+        bash_command='sleep 10 && echo "$(date +T%H:%M:%S)"'
     )
 
     extract_b = BashOperator(
         task_id='extract_b',
-        bash_command='sleep 1'
+        bash_command='sleep 10 && echo "$(date +T%H:%M:%S)"'
     )
 
     load_a = BashOperator(
         task_id='load_a',
-        bash_command='sleep 1'
+        bash_command='sleep 10 && echo "$(date +T%H:%M:%S)"'
     )
 
     load_b = BashOperator(
         task_id='load_b',
-        bash_command='sleep 1'
+        bash_command='sleep 10 && echo "$(date +T%H:%M:%S)"'
     )
 
     transform = BashOperator(
         task_id='transform',
-        bash_command='sleep 1'
+        bash_command='sleep 10 && echo "$(date +T%H:%M:%S)"'
     )
 
     extract_a >> load_a
